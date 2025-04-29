4d8240f67dd2685f5dcc2d261f4c73bd4d700b8a
Angel <53478143+angel-langdon@users.noreply.github.com>
2025-04-29 20:52:40 +0200

    Enhance JIRA task export functionality and update dependencies
    - Implemented JIRA issue fetching and data processing in main.py
    - Added duration formatting for time spent on tasks
    - Updated .gitignore to include .env files
    - Added required dependencies for dotenv and requests in pyproject.toml and uv.lock

--- main.py
+++ main.py
@@ -1,22 +1,83 @@
-import sys
-from datetime import datetime, timedelta
+import os
+from datetime import datetime
+from decimal import Decimal
 from pathlib import Path
 
 import pandas as pd
 import pdfkit
+import requests
+from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+
+
+def duration(seconds: int) -> str:
+    """Convert seconds to a human-readable format."""
+    if pd.isna(seconds):
+        return "0h 0m"
+    hours, remainder = divmod(seconds, 3600)
+    minutes, _ = divmod(remainder, 60)
+    return f"{hours:.0f}h {minutes:.0f}m"
+
+
+load_dotenv(override=True)
+
+JIRA_URL = os.environ["JIRA_URL"]  # "https://your-domain.atlassian.net"
+EMAIL = os.environ["EMAIL"]
+API_TOKEN = os.environ["API_TOKEN"]
+PROJECT_KEY = os.environ["PROJECT_KEY"]
+HOURLY_RATE = Decimal(os.environ["HOURLY_RATE"])
+CURRENCY = os.environ["CURRENCY"]
+
+headers = {"Accept": "application/json", "Content-Type": "application/json"}
+
+search_url = f"{JIRA_URL}/rest/api/3/search"
+auth = HTTPBasicAuth(EMAIL, API_TOKEN)
+jql_query = f'project = "{PROJECT_KEY}" ORDER BY updated ASC'
+fields = "project,summary,updated,timespent"
+rename_map = {
+    "project": "Project name",
+    "summary": "Summary",
+    "timespent": "Time Spent",
+    "updated": "Updated",
+    "taskcost": f"Task Cost (hours x{HOURLY_RATE}{CURRENCY}/hour",
+}
+dt_cols = ["updated"]
+params = {"jql": jql_query, "fields": fields, "maxResults": 100}
+
+
+def get_jira_issues():
+    """Fetch issues from JIRA."""
+    response = requests.get(search_url, headers=headers, params=params, auth=auth)
+    response.raise_for_status()
+
+    issues = response.json()["issues"]
+    data = []
+    for issue in issues:
+        dic = issue["fields"]
+        dic["project"] = dic["project"]["name"]
+        data.append(dic)
+
+    df = pd.DataFrame(data)
+    for c in dt_cols:
+        df[c] = pd.to_datetime(df[c]).dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    df["taskcost"] = [
+        (Decimal(v) / Decimal(3600)) * HOURLY_RATE for v in df["timespent"]
+    ]
+    df["taskcost"] = df["taskcost"].fillna(Decimal(0))
+    df["timespent"] = [duration(s) for s in df["timespent"]]
+    df = df[rename_map.keys()]
+    return df
+
+
+get_jira_issues()
+# %%
 
 if __name__ == "__main__":
-    path = Path(sys.argv[1])
-    df = pd.read_csv(path)
-    dt_cols = ["Created", "Updated", "Resolved"]
-    time_cols = ["Time Spent", "Σ Time Spent"]
-    keep_cols = ["Summary", "Project name", "Time Spent", "Σ Time Spent", *dt_cols]
-    for col in time_cols:
-        df[col] = [
-            str(timedelta(seconds=int(v))) if pd.notna(v) else v for v in df[col]
-        ]
-    df = df[keep_cols]
-
+    df = get_jira_issues()
+    total_cost = df["taskcost"].sum()
+    df["taskcost"] = df["taskcost"].apply(lambda x: f"{x:.2f} {CURRENCY}")
+    df = df.rename(columns=rename_map)
     html_styles = """<style>
         table {
             border-collapse: collapse;
@@ -47,6 +108,8 @@ if __name__ == "__main__":
     <p>Generated on {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}</p>
 
     {df.to_html(index=False)}
+
+    <h3>Total cost {total_cost:.2f}{CURRENCY}</h3>
     """
 
     pdf_path = Path("report.pdf")
@@ -63,3 +126,4 @@ if __name__ == "__main__":
         },
     )
     print(f"PDF report generated at {pdf_path.absolute()}")
+    os.startfile(pdf_path)
